26be74225c010fa9c240fda263192c343ec01d4e
Anton <antonbakum2002@gmail.com>
2024-04-18 12:48:24 +0300

    commit

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-const form = document.getElementById("form");
+const newPage = document.getElementById("new")
+const checkPage = document.getElementById("check")
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const values = Object.fromEntries(formData.entries());
-  const { myPart, total, tip } = values;
-  const toPay = +myPart + (+myPart / +total) * +tip;
-  document.getElementById("result").innerHTML = `Плоти ${toPay.toFixed(2)}`;
-});
+
+const queryParams = new URL(location).searchParams;
+const name = queryParams.get("name")
+const totalSum = +queryParams.get("totalSum")
+const tip = +queryParams.get("tip")
+
+
+const isNewPage = !name;
+
+if (isNewPage) newPage.classList.remove("hidden")
+else checkPage.classList.remove("hidden")
+
+const newForm = document.getElementById("newForm")
+
+newForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const data = new FormData(e.target)
+  const entries = Object.fromEntries(data.entries())
+  const url = new URL(location);
+  url.searchParams.append("name", entries['name'])
+  url.searchParams.append("totalSum", entries['totalSum'])
+  url.searchParams.append("tip", entries['tip'])
+  location.href = url;
+})
+
+const checkForm = document.getElementById("checkForm")
+
+checkForm.addEventListener("submit", e => {
+  e.preventDefault()
+  const data = new FormData(e.target)
+  const entries = Object.values(Object.fromEntries(data.entries()))
+
+  const amount = entries.reduce((acc, e) => acc + +eval(e), 0)
+  const ratio = amount / totalSum
+  const tipPart = tip * ratio
+  const resultAmount = Math.ceil(tipPart + amount);
+
+  document.getElementById("result").innerText = resultAmount
+
+})
+let items = 0;
+const addNewItem = document.getElementById("addNewItem")
+
+addNewItem.addEventListener("click", e => {
+  const item = document.createElement("input")
+  item.classList = "h-8 border-black border-2 border-solid rounded px-4 text-md"
+  item.name = ++items
+  checkForm.insertBefore(item, addNewItem)
+  item.focus();
+})
